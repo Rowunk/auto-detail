0b2b67237713b940d3ce4947b8dcb1965635e880
William Eberlein <william.eberlein@gmail.com>
2025-08-02 06:40:43 +0200

    chore(jest): add file mock, install ts-jest deps and update config

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,21 @@
 // jest.config.ts
 import type { Config } from '@jest/types';
 
-/**
- * Jest configuration in ESM format.
- */
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1',
-    },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    // mock CSS imports:
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    // mock media assets:
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.ts'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
 };
 
 export default config;
